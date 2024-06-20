8e0ee1ca72c5efd90cba0b613b31a320901e4584
Sanjay Tiwari <Sanjutiwari9559@gmail.com>
2024-06-20 16:35:35 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AsyncAndAwait
+{
+    internal class Program
+    {
+        static  async void  Main(string[] args)
+        {
+            string message= await DoWorkAsync();
+            workDone();
+                                                 //If U are Using await then It Must Be Async Function and It can Be Return Task Or Not 
+        }                                       //This Is Async call and Wating call and Non Blocking  call
+        public static async Task<String> DoWorkAsync()
+        {
+            return  await Task.Run(() => { Thread.Sleep(1000);return "WorkDone"; });
+        }
+        public static void workDone()
+        {
+            Console.WriteLine("This is Work Done ");
+        }
+    }
+}
